fe90e0145d6dee675a7c0a6e16ccff5bc603a5ac
Koji Kuno <koji.kuno@gmail.com>
2024-08-14 18:54:39 +0900

    パスワードプロテクトブロックの試作（まだできてない）

---
+++ plugins/password-protected-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2cf6e6898a9e99a679ef');
---
+++ plugins/password-protected-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '77bfb81d9632188ff321');
